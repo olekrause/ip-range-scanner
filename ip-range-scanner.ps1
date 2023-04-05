3c2202e3ce6d0316bff6dd17b6b9a2f502790a97
olekrause <aiciquiu300@gmail.com>
2023-04-05 17:35:08 +0200

    Added comments
    With GPT-4! Worked like a charm :)

--- ip-range-scanner.ps1
+++ ip-range-scanner.ps1
@@ -1,38 +1,51 @@
 ﻿<#
 .SYNOPSIS
-	Scan an entire class c network.
+	Scans a Class C network subnet for active hosts.
 .DESCRIPTION
-	This script is able to ping an entire class c network.
-	When done, it will output the IP-addresses and hostnames that were found on the network to the console.
+	This script pings each IP address within a specified Class C network subnet.
+	It then outputs the IP addresses and hostnames (if available) of active hosts to the console.
 #>
 
-# IP-address from the network you are trying to scan. Doesn't need to be XXX.XXX.XXX.0
-$ip = "10.99.10.0"
+# Set the initial IP address
+$ip = "10.108.60.0"
 
-
-
-
-
-
-
+# Remove any existing jobs
 Get-Job | Remove-Job
 
+# Extract the subnet from the IP address using regex
 $ip -match '\d+.\d+.\d+'
 $Matches[0]
+
+# Loop through 0 to 254 (255 iterations)
 for ($i = 0; $i -lt 255; $i++) {
+	# Create the current IP address by combining the subnet and the current iteration
 	$range = $Matches[0], $i -join "."
+	
+	# Ping the IP address once and create a job for it, suppressing any errors
 	Test-Connection -ComputerName $range -Count 1 -ErrorAction Stop -AsJob
 }
 
+# Wait for all jobs to complete
 Get-Job | Wait-Job
 
+# Clear the console screen
+Clear-Host
+
+# Loop through the completed jobs
 foreach ($job in Get-Job) {
-	$job_result = Receive-Job $Job -Keep
+	# Get the job result
+	$job_result = Receive-Job $Job
+
+	
+	# If the IP address is online (response time is greater or equal to 0)
 	if ($job_result.responsetime -ge 0) {
+		# Try to resolve the DNS name of the IP address, suppressing any errors
 		if (Resolve-DnsName $job_result.Address -ErrorAction Ignore) {
-			Write-Host $job_result.Address	((Resolve-DnsName $job_result.Address -ErrorAction Ignore).NameHost).TrimEnd(".stadt.wolfsburg.de")
+			# Display the IP address and the trimmed DNS name without the domain suffix
+			Write-Host $job_result.Address "	" ((Resolve-DnsName $job_result.Address -ErrorAction Ignore).NameHost).TrimEnd(".stadt.wolfsburg.de")
 		}
 		else {
+			# Display just the IP address if no DNS name is found
 			Write-Host $job_result.Address
 		}
 	} 
